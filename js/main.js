f55cb69b803dae17459c9bdd0d7efc8c59327fd7
Игорь <igorfendrih@mail.ru>
2021-10-09 16:06:26 +0500

    Исправления 4 раздела 1 задания

--- js/main.js
+++ js/main.js
@@ -1,35 +1,7 @@
-const getRandomInt = (min, max, counter = 0) => {
-  if (min > max || min < 0 || max <= 0) {
-    return false;
-  }
-  return +(Math.random() * (max - min) + min).toFixed(counter);
-};
-
-console.log(getRandomInt(0, 10, 5));
-
-const getAvatar = () => {
-  const avatarId = getRandomInt(1, 9);
-  const author = {
-    avatar: `img/avatars/user${avatarId}.png`,
-  };
-  return author;
-};
-
-
-const PRICE = getRandomInt(100, 1000);
-
-const TITLE = [' Недвижимость в вашем городе '];
-
+const OFFERS_COUNT = 10;
+const TITLE = 'Недвижимость в вашем городе';
 const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-
-const ROOMS = getRandomInt(1, 100);
-
-const GUESTS = getRandomInt(1, 200);
-
-const CHECKINS = ['12:00', '13:00', '14:00'];
-
-const CHECKOUTS = ['12:00', '13:00', '14:00'];
-
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = [
   'wifi',
   'dishwasher',
@@ -38,51 +10,59 @@ const FEATURES = [
   'elevator',
   'conditioner',
 ];
-
-const DESCRIPTIONS = ['big', 'small', 'average'];
-
+const DESCRIPTIONS = [
+  'Прямой железнодорожный маршрут до аэропорта Ханеда. Всего в 5 минутах ходьбы!',
+  'В окрестностях есть много ресторанов. Популярный китайский ресторан Nihao, Ootoya, Burger King и т.д.',
+  'Безключевая регистрация заезда (Smart lock system)',
+  'Полностью оборудована мебелью, бытовой техникой, кухней и удобствами. Идеально подходит для длительного пребывания!',
+  'Коробка доставки и почтовый ящик доступны!',
+  'Фантастический доступ в аэропорт Ханеда, Шинагава, Кавасаки и Йокогама]',
+];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-
-const OFFERS_COUNT = 1;
+const getRandomInt = (min, max, counter = 0) => {
+  if (min > max || min < 0 || max <= 0) {
+    return false;
+  }
+  return +(Math.random() * (max - min) + min).toFixed(counter);
+};
+const getAvatar = () => {
+  const avatarId = getRandomInt(1, 9);
+  const author = {
+    avatar: `img/avatars/user${avatarId}.png`,
+  };
+  return author;
+};
 
 const getRandomOfferElement = (elements) =>
   elements[getRandomInt(0, elements.length - 1)];
-
 const createOffer = () => {
-
   const lat = getRandomInt(35.65, 35.7, 5);
   const lng = getRandomInt(139.7, 139.8, 5);
 
   return {
-
-
     author: getAvatar(),
-
     offer: {
       title: TITLE,
       address: `${lat} ${lng}`,
-      price: getRandomInt(PRICE),
+      price: getRandomInt(0, 1000000),
       type: getRandomOfferElement(TYPE),
-      rooms: getRandomInt(ROOMS),
-      guests: getRandomInt(GUESTS),
-      checking: getRandomOfferElement(CHECKINS),
-      checkout: getRandomOfferElement(CHECKOUTS),
+      rooms: getRandomInt(1, 3),
+      guests: getRandomInt(1, 3),
+      checking: getRandomOfferElement(CHECK_TIMES),
+      checkout: getRandomOfferElement(CHECK_TIMES),
       features: getRandomOfferElement(FEATURES),
       description: getRandomOfferElement(DESCRIPTIONS),
       photos: getRandomOfferElement(PHOTOS),
     },
-
     location: {
       lat,
       lng,
     },
   };
 };
-
 const similarOffers = Array.from({ length: OFFERS_COUNT }, createOffer);
-
 console.log(similarOffers);
